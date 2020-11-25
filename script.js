12f17ebb3cd991b946f25cbf12dcdacfb7f8f6e2
Mandi <haase1020@gmail.com>
2020-11-25 07:51:37 -0500

    adding javascript

---
+++ script.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('form');
+const password1El = document.getElementById('password1');
+const password2El = document.getElementById('password2');
+const messageContainer = document.querySelector('.message-container');
+const message = document.getElementById('message');
+
+let isValid = false;
+
+function validateForm() {
+    //using constraint API
+    isValid = form.checkValidity();
+    
+    //style main message for error
+    message.textContent = "Please fill out all fields 🙂.";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+
+}
+
+
+function processFormData(e) {
+    e.preventDefault();
+    // validate form
+    validateForm();
+}
+// event listener
+form.addEventListener('submit', processFormData);
