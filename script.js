0276c03e6d1f5c01f9ec80dd667b54ff949fafde
Mandi <haase1020@gmail.com>
2020-11-26 08:07:24 -0500

    complete project

--- script.js
+++ script.js
@@ -10,18 +10,59 @@ function validateForm() {
     //using constraint API
     isValid = form.checkValidity();
     
-    //style main message for error
-    message.textContent = "Please fill out all fields 🙂.";
-    message.style.color = "red";
-    messageContainer.style.borderColor = "red";
+
+    if(!isValid) {
+        //style main message for error
+        message.textContent = "Please fill out all fields 🙂.";
+        message.style.color = "red";
+        messageContainer.style.borderColor = "red";
+        return;
+    }
+  //check to see if passwords match
+  if (password1El.value === password2El.value) {
+      passwordsMatch = true;
+      password1El.style.borderColor = "green";
+      password2El.style.borderColor = "green";
+  } else {
+      passwordsMatch = false;
+      message.textContent = "Make sure passwords match 🙂."
+      message.style.color = "red";
+      messageContainer.style.borderColor = "red";
+      password1El.style.borderColor = "red";
+      password2El.style.borderColor = "red";
+      false;
+  }
+  // if form is valid and passwords match
+  if (isValid && passwordsMatch) {
+      message.textContent = "SUCCESS REGISTERING 🎉!!";
+      alert("SUCCESS🎉!");
+      messageContainer.style.borderColor = "green";
+  }
 
 }
 
+
+function storeFormData() {
+    const user = {
+        name: form.name.value,
+        phone: form.phone.value,
+        email: form.email.value,
+        website: form.website.value,
+        password: form.password.value
+    }
+
+    //do something with user data
+    console.log(user);
+}
 
 function processFormData(e) {
     e.preventDefault();
     // validate form
     validateForm();
+    //submit data if valid
+    if (isValid && passwordsMatch) {
+        storeFormData();
+    }
 }
 // event listener
 form.addEventListener('submit', processFormData);
